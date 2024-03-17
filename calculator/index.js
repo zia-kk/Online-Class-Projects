1c1f841499d3b4cab0848555847713e121ebb897
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-17 21:25:38 +0500

    project calculator

---
+++ calculator/index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first Numner", type: "number", name: "firstnumber" },
+    { message: "Enter your second Number", type: "number", name: "secondnumber" },
+    { message: "Please kisi aik operator ko choose karein",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtraction", "multiplication", "division", "modulus"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log("Anser=", answer.firstnumber + answer.secondnumber);
+}
+if (answer.operator === "subtraction") {
+    console.log("Anser=", answer.firstnumber - answer.secondnumber);
+}
+if (answer.operator === "multiplication") {
+    console.log("Anser=", answer.firstnumber * answer.secondnumber);
+}
+if (answer.operator === "division") {
+    console.log("Anser=", answer.firstnumber + answer.secondnumber);
+}
+if (answer.operator === "modulus") {
+    console.log("Anser=", answer.firstnumber % answer.secondnumber);
+}
