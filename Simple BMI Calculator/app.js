a496c40cf7dca3f3055b1c0135d5ab11d145861c
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-17 21:42:50 +0500

    BMI

---
+++ Simple BMI Calculator/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let weightofBody = 85; //85kg
+let heighofBody = 1.5; //1.5m
+let BMI = weightofBody / (heighofBody * heighofBody);
+console.log("Your BMI is=", BMI);
---
+++ Simple BMI Calculator/app.ts
@@ -0,0 +1,5 @@
+let weightofBody= 85  //85kg
+let heighofBody= 1.5  //1.5m
+let BMI  = weightofBody/(heighofBody*heighofBody);
+
+console.log("Your BMI is=",BMI)
