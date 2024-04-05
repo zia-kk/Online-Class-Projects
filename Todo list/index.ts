a29348bab7e6ba972fca901b434710e025df2e70
Zia Ur Rehman <pkzia112@gmail.com>
2024-04-05 18:25:56 +0500

    Simple TODO list project

---
+++ Todo list/index.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+
+let todos=[]
+
+let conndition=true;
+console.log("\n");
+
+console.log(chalk.bold.bgMagentaBright("\t\t\t >>>>>>Welcome To TODO App<<<<<\t" ))
+console.log("\n");
+
+while(conndition){
+
+let todolist = await inquirer.prompt([
+    {
+        name: "firstQuestion",
+        type: "input",
+        message: chalk.hex('F57A00')("what would you like to add todos:"),
+    },
+
+    {
+        name: "secondQuestion",
+        type: "confirm",
+        message:chalk.bold.hex('FF0000')("would you like to add more in you todo?"),
+        default: "true",
+    }
+]);
+
+todos.push(todolist.firstQuestion);
+
+console.log(todos);
+conndition= todolist.secondQuestion;
+}
